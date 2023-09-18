cf0f7b82ca9dc83d60f00961c80cbdff93ed5841
nickb <nicolobartolinii@gmail.com>
2023-09-18 12:32:30 +0200

    seeders rooms, guests, reservations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,29 +16,49 @@ class DatabaseSeeder extends Seeder {
         // Elimina gli altri seeder se non ti servono
         // Ho mantenuto il seeder per 'users' perché potrebbe essere utile per creare account di test
 
-        DB::table('prenotaziones')->insert([
+        DB::table('guests')->insert([
             [
-                'nome_cliente' => 'Mario Rossi',
-                'email_cliente' => 'mario.rossi@example.com',
-                'telefono_cliente' => '1234567890',
-                'data_inizio' => '2023-09-20',
-                'data_fine' => '2023-09-25',
-                'numero_persone' => 2,
-                'created_at' => now(),
-                'updated_at' => now()
+                'first_name' => 'Mario',
+                'last_name' => 'Rossi',
+                'email_address' => null,
+                'phone_number' => null,
             ],
             [
-                'nome_cliente' => 'Luca Verdi',
-                'email_cliente' => 'luca.verdi@example.com',
-                'telefono_cliente' => '0987654321',
-                'data_inizio' => '2023-09-26',
-                'data_fine' => '2023-09-30',
-                'numero_persone' => 1,
-                'created_at' => now(),
-                'updated_at' => now()
-            ],
-            // ... aggiungi altre prenotazioni di prova se necessario
+                'first_name' => 'Luca',
+                'last_name' => 'Verdi',
+                'email_address' => null,
+                'phone_number' => null,
+            ]
         ]);
-
+        DB::table('rooms')->insert([
+            [
+                'name' => 'Camera 1',
+                'capacity' => 2,
+            ],
+            [
+                'name' => 'Camera 2',
+                'capacity' => 1,
+            ]
+            ]);
+            DB::table('reservations')->insert([
+                [
+                    'guest_id' => 1,
+                    'room_id' => 1,
+                    'arrival_date' => '2023-09-20',
+                    'departure_date' => '2023-09-25',
+                    'number_of_guests' => 2,
+                    'under_14' => 0,
+                    'amount_per_night' => 100.00,
+                ],
+                [
+                    'guest_id' => 2,
+                    'room_id' => 2,
+                    'arrival_date' => '2023-09-26',
+                    'departure_date' => '2023-09-30',
+                    'number_of_guests' => 1,
+                    'under_14' => 0,
+                    'amount_per_night' => 100.00,
+                ]
+                ]);
     }
 }
