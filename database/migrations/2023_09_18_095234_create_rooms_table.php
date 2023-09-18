437c65c2ab905e82efb984a3d015f56b29dae8f4
Nicola Picciafuoco <s1099360@studenti.univpm.it>
2023-09-18 12:09:01 +0200

    aggiunti 2 tabelle

--- database/migrations/2023_09_18_095234_create_rooms_table.php
+++ database/migrations/2023_09_18_095234_create_rooms_table.php
@@ -13,17 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('camera', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nome_cliente');
-            $table->string('email_cliente');
-            $table->string('telefono_cliente');
-            $table->date('data_inizio');
-            $table->date('data_fine'); // Assicurati di avere questa linea
-            $table->integer('numero_persone');
+            $table->string('name'); // Nome
+            $table->integer('capacity'); // Capienza
             $table->timestamps();
         });
-
     }
 
     /**
@@ -33,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('prenotaziones');
+        Schema::dropIfExists('rooms');
     }
 };
